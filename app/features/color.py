8485b288ce884a185779dc9552980e1c6b822b2b
Sam <sam_mie_sam@hotmail.com>
2016-10-04 16:02:01 +0200

    restructured color feature code to use an object because OO is strict better.

--- app/features/color.py
+++ app/features/color.py
@@ -7,54 +7,59 @@ import numpy
 ################################################################################
 # FUNCTIONS
 ################################################################################
-def generateHlsHistogram(hlsimg):
-    """
-    Generates a histogram based on the given HLS image.
-    The histogram contains 20 bins for different hue values and 1 bin for greyscale.
-    """
-    #Algorithm parameters
-    #Note that in OpenCV hls uses the ranges [0,179], [0,255] and [0,255] respectively
-    saturationThreshold = (int)(.2 * 255)
-    minLightness = (int)(.15 * 255)
-    maxLightness = (int)(.95 * 255)
+class ColorFeatureExtracter:
+    _histogram = 0;
+    _opencvimg = 0;
+    _hlsimg = 0;
     
+    def __init__(self, opencvimage):
+        self._opencvimg = opencvimage
     
-    histogram = [0] * 21
-    [width, height, depth] = hlsimg.shape
+    def getHlsHistogram(self):
+        """
+        Generates a histogram based on the given HLS image.
+        The histogram contains 20 bins for different hue values and 1 bin for greyscale.
+        """
+        if self._histogram:
+            return self._histogram
+        hlsimg = self.getHlsImage()
+        #Algorithm parameters
+        #Note that in OpenCV hls uses the ranges [0,179], [0,255] and [0,255] respectively
+        saturationThreshold = (int)(.2 * 255)
+        minLightness = (int)(.15 * 255)
+        maxLightness = (int)(.95 * 255)
+        
+        
+        histogram = [0] * 21
+        [width, height, depth] = hlsimg.shape
+        
+        for y in range(0, height):
+            for x in range(0, width):
+                #If not greyscale
+                if hlsimg.item(x, y, 2) > saturationThreshold or minLightness < hlsimg.item(x, y, 1) < maxLightness:
+                    histogram[hlsimg.item(x, y, 0) // 9] += 1
+                #Else
+                else:
+                    histogram[20] += 1
+        self._histogram = histogram
+        return histogram
     
-    for y in range(0, height):
-        for x in range(0, width):
-            #If not greyscale
-            if hlsimg.item(x, y, 2) > saturationThreshold or minLightness < hlsimg.item(x, y, 1) < maxLightness:
-                histogram[hlsimg.item(x, y, 0) // 9] += 1
-            #Else
-            else:
-                histogram[20] += 1
-    return histogram
+    def getHlsImage(self):
+        if not self._hlsimg:
+            self._hlsimg = cv2.cvtColor(self._opencvimg, cv2.COLOR_BGR2HLS)
+        return self._hlsimg
+    
+    def getHueCount(self):
+        #Algorithm parameters
+        noiseThreshold = .05
 
-
-def huefeatures(image):
-    """
-    Function that returns the amount of different colours detected within the image.
-
-    Takes a OpenCV image
-    Returns the number of different colours
-
-    Feature from: 'The Design of High-Level Features for Photo Quality Assessment' by Y.Ke et al. (2006)
-
-    Coded by Sam
-    """
-    #Algorithm parameters
-    noiseThreshold = .05
-
-    #Convert image
-    hlsimg = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
-    histogram = generateHlsHistogram(hlsimg)
-
-    #Return the number of colours that pass the threshold (threshold relative to max count)
-    maxColor = max(histogram)
-    return [len([x for x in histogram if x > maxColor * noiseThreshold]),(histogram.index(maxColor)*9)+10, maxColor]
-
+        histogram = self.getHlsHistogram()
+        maxColor = max(histogram)
+        return len([x for x in histogram if x > maxColor * noiseThreshold])
+    
+    def getMostCommonColor(self):
+        histogram = getHlsHistogram()
+        return histogram.index(max(histogram))
 
 ################################################################################
 # TESTS
@@ -69,7 +74,8 @@ class TestColorFeatures(unittest.TestCase):
         thispath = os.path.dirname(__file__)
         impath = os.path.join("test", "opencv-logo.png")
         img = cv2.imread(os.path.join(thispath, impath))
-        count = huefeatures(img)[0]
+        colorextr = ColorFeatureExtracter(img)
+        count = colorextr.getHueCount()
         self.assertEqual(count, 4)
         # ... and then evaluate the output
 
